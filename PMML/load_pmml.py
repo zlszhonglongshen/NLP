de9f93681cdc332f60b2f8e4532117b728d24f74
zhonglongshen <593956670@qq.com>
2018-12-26 23:03:08 +0800

    update

---
+++ PMML/load_pmml.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 18:30:11 2018
+
+@author: johnson.zhong
+"""
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from sklearn_pmml_model.tree import PMMLTreeClassifier
+
+# Prepare data
+iris = load_iris()
+
+# We only take the two corresponding features
+X = pd.DataFrame(iris.data)
+X.columns = np.array(iris.feature_names)
+y = pd.Series(np.array(iris.target_names)[iris.target])
+y.name = "Class"
+
+Xtr, Xte, ytr, yte = train_test_split(X, y, test_size=0.33, random_state=123)
+
+df = pd.concat([Xte, yte], axis=1)
+
+clf = PMMLTreeClassifier(pmml="models/sklearn2pmml.pmml")
+clf.predict(Xte)
+clf.score(Xte, yte)
